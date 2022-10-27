1794798cf131b8517706a3881aebd3b6b0763d43
Andre Ferreira <Andre.Ferreira@paulsmith.co.uk>
2022-10-27 14:51:52 +0100

    routes and page rendering

---
+++ resources/js/Pages/Another.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>{{$page.props.appName}}</p>
+    <div>
+        This is another page
+    </div>
+    <Link :href="route('home')">Home page</Link>
+</template>
+
+
+<script>
+    import { Link } from '@inertiajs/inertia-vue3'
+
+export default {
+
+    components: {
+        Link
+    }
+
+}
+
+
+</script>
